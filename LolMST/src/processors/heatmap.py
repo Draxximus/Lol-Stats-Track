da27b121ab38def7b303159cbfe78ff78ee68916
Draxximus <draxxstudy@gmail.com>
2025-11-28 10:44:11 +0000

    Update heatmap.py

--- LolMST/src/processors/heatmap.py
+++ LolMST/src/processors/heatmap.py
@@ -23,7 +23,7 @@ def create_mouse_heatmap(
     ys = [e["y"] for e in filtered if "y" in e]
 
     if not xs or not ys:
-        print("⚠ No cords found in the events list.")
+        print("No cords found in the events list.")
         return
     min_y = min(ys)
     min_x = min(xs)
@@ -34,22 +34,16 @@ def create_mouse_heatmap(
     print(f"[HEATMAP] Rango Y: {min_y} .. {max_y}")
 
     if min_x == max_x or min_y == max_y:
-        print("⚠ El rango de coordenadas es demasiado pequeño para un heatmap.")
+        print("El rango de coordenadas es demasiado pequeño para un heatmap.")
         return
 
-    # 3) Normalizamos coordenadas para que empiecen en 0
-    #    Ejemplo: si X está entre -1500 y -500 -> lo pasamos a 0..1000
     xs_norm = [x - min_x for x in xs]
     ys_norm = [y - min_y for y in ys]
 
     width = max_x - min_x
     height = max_y - min_y
 
-    # 4) Creamos la carpeta destino si no existe
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
-
-    # 5) Creamos el histograma 2D
-    #    OJO: ahora cada rango sí tiene [min, max]
     heatmap, xedges, yedges = np.histogram2d(
         xs_norm,
         ys_norm,
@@ -73,4 +67,5 @@ def create_mouse_heatmap(
     plt.close()
 
     print(f"Heatmap saved to {output_path}")
+
 
